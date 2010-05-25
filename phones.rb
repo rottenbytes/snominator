d4c4059ec7dd8b7fa16fb6c293f8c3b085085228
Nico <nico@rottenbytes.info>
2010-05-25 12:17:18 +0200

    Added /config/MAC & modified README

--- phones.rb
+++ phones.rb
@@ -12,7 +12,7 @@ DB = Sequel.connect(:adapter => "mysql",
                     :password => c["password"] )
 
 l=DB[:md_phones]
-k=DB[:md_phones_keys]
+#k=DB[:md_phones_keys]
 
 get '/' do
     @phones = l.order(:sipnumber)
@@ -30,7 +30,8 @@ end
 
 post '/:phonenumber' do
     id = params[:id]
-      
+    
+    # update these fields  
     [:sipnumber, :nom, :email, :sippassword, :mac].each { |entry|
         l.filter(:id => id).update(entry => params[entry])
     }
@@ -39,9 +40,36 @@ post '/:phonenumber' do
 end
 
 get '/config/:mac' do
+    content_type 'text/plain'
+
     mac = params[:mac]
     @phone = l.filter[:mac => mac]
     
+    # Show config here (with haml, pure text)
+    config = "language&: Francais
+tone_scheme!: GER
+no_dnd&: on\n"
+    config += "ntp_server!: " + c["ntpserver"]+"\n"
+    config += "dhcp&: on
+silence_compression!: off
+timezone&: FRA+1
+date_us_format&: off
+time_24_format&: on
+user_active1!: on\n"
+    config += "user_name1!: "+ (@phone[:sipnumber].to_s) +"\n"
+    config += "user_host1!: "+ c["sipserver"] +"\n"
+    config += "user_pname1!: "+ (@phone[:sipnumber].to_s) +"\n"
+    config += "user_mailbox1!: *1\n"
+    config += "user_idle_text1!: "+ (@phone[:nom].to_s) +"\n"
+    config += "user_outbound1!: "+ c["sipserver"]+"\n"
+    config += "record_missed_calls1!: on\n"
+    config += "user_pass1!: "+ (@phone[:sippassword].to_s) +"\n"
+    config += "phone_name!: "+ (@phone[:nom].to_s) +"\n"
+    config += "user_realname1!: "+ (@phone[:nom].to_s) +"\n"
+
+
+    config
+
 end
 
 
